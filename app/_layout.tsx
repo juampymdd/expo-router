019de4ff4a6f083a587fb297c775b4947dc2fabe
Juan Pablo Maddoni <juampymdd@gmail.com>
2025-07-27 22:08:01 -0300

    chore: setup NativeWind and Tailwind CSS configuration
    - Added babel.config.js to configure Babel with NativeWind preset.
    - Created metro.config.js to integrate NativeWind with Expo's Metro bundler.
    - Added nativewind-env.d.ts for TypeScript support with NativeWind.
    - Configured tailwind.config.js to define custom theme colors and font families, and set content paths for Tailwind CSS classes.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen } from "expo-router";
+import React from "react";
+import "./global.css";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "WorkSans-Black": require("../assets/fonts/WorkSans-Black.ttf"),
+    "WorkSans-Light": require("../assets/fonts/WorkSans-Light.ttf"),
+    "WorkSans-Medium": require("../assets/fonts/WorkSans-Medium.ttf"),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
+  if (!fontsLoaded) {
+    if (error) {
+      console.error("Error loading fonts:", error);
+      throw new Error("Failed to load fonts: " + error.message);
+    }
   }
+  SplashScreen.hideAsync();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+  return <Slot />;
+};
+
+export default RootLayout;
